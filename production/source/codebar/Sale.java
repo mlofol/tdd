8bb07c7270a2363c1d9fcdc245336cb256a9b211
Carmen Lopez <carmen.lopez@gmail.com>
2020-12-11 07:37:17 +0100

    Removed temporal duplication in production code

--- production/source/codebar/Sale.java
+++ production/source/codebar/Sale.java
@@ -5,16 +5,18 @@ import java.util.Map;
 
 public class Sale {
     private Display display;
+    private final Map<String, String> pricesByBarcode;
 
     public Sale(Display display) {
         this.display = display;
+        pricesByBarcode = new HashMap<>();
+        pricesByBarcode.put("12345", "7.6euros");
+        pricesByBarcode.put("12346", "0.6euros");
     }
 
     public void onBarcode(String barcode) {
 
-        Map<String, String> pricesByBarcode = new HashMap<>();
-        pricesByBarcode.put("12345", "7.6euros");
-        pricesByBarcode.put("12346", "0.6euros");
+
         if ("".equals(barcode)) {
             display.setPrice("Scanning error: empty barcode");
         } else if (pricesByBarcode.containsKey(barcode)) {
