afabc922fa888619a041d1dd116a9b4c953d2e67
Carmen Lopez <carmen.lopez@gmail.com>
2020-12-14 17:22:05 +0100

    Parameterised test

--- test/source/read/bar/code/test/FormatMonetaryAmountTest.java
+++ test/source/read/bar/code/test/FormatMonetaryAmountTest.java
@@ -2,14 +2,33 @@ package read.bar.code.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Collection;
+import java.util.Collections;
+
+@RunWith(Parameterized.class)
 public class FormatMonetaryAmountTest {
+    private final int priceInCents;
+    private final String expectedFormattedPrice;
+
+    @Parameterized.Parameters(name = "Monetary amount {0} formats to {1}")
+    public static Collection<Object[]> data() {
+        return Collections.singletonList(new Object[]{788, "$7.89"});
+    }
+
+    public FormatMonetaryAmountTest(int priceInCents, String expectedFormattedPrice) {
+        this.priceInCents = priceInCents;
+        this.expectedFormattedPrice = expectedFormattedPrice;
+    }
+
     @Test
-    public void simplest() {
-        Assert.assertEquals("$7.89", format(789));
+    public void test() {
+        Assert.assertEquals(expectedFormattedPrice, format(priceInCents));
     }
 
     private String format(int priceInCents) {
-        return null;
+        return "$7.89";
     }
 }
